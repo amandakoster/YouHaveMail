a8286767192c2766edbda17543cf511b5511186e
Amanda Koster <amandakoster@gmail.com>
2017-03-27 14:30:21 -0700

    Working on JS

--- RegistrationApp.js
+++ RegistrationApp.js
@@ -12,6 +12,17 @@ function createNewUser(event) {
   tFoot.removeChild(totalsRow);
   hourlyTotals();
   form.reset();
-  }
+}
 
-var localstorage =
+var localstorage = document.getElementById().valye;
+
+// The localStorage object has a setItem method which is used to store an item. It takes 2 arguments - a key by which you can refer to the item, and a value:
+var input = document.getElementById('saveServer');
+localStorage.setItem('server', input.value);
+// The above code first gets a reference to the input element, and then stores an item ("server") in local storage with the value of the value of that input element.
+
+// You can retrieve the value by calling getItem:
+var storedValue = localStorage.getItem('server');
+
+// For getting the text back:
+document.getElementById('saveServer').value = localStorage.getItem('server');
