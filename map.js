9efe62604f2455403e93d1e4a0a1411c4e1c0bce
James Salamonsen <jamessalamonsen@gmail.com>
2017-03-27 15:08:11 -0700

    I got my functions on this part of my site working great and storing the input.

--- map.js
+++ map.js
@@ -2,5 +2,6 @@ document.getElementById("markerSubmitButton").addEventListener("click", markerSu
 function markerSubmit() {
 	var markerInput = document.getElementById('markerInput').value;
 	localStorage.setItem('submittedInput', markerInput);
+	alert("Messages have been sent to marker " + markerInput +". Which include houses A,B,C and D.");
 	document.getElementById("markerInput").value = "";
 }
