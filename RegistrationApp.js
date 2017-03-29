84d80e9eea0f400ef078101b6b60067f36b21b60
Amanda Koster <amandakoster@gmail.com>
2017-03-29 13:12:53 -0700

    working on adding checkbox value to local storage

--- RegistrationApp.js
+++ RegistrationApp.js
@@ -1,28 +1,4 @@
 'use strict';
-//
-// var x = document.getElementById('smsChecked');
-// var y = document.getElementById('emailChecked');
-//
-// //when smsSelect is ckecked, sent that boolan value to local storage.
-//
-// if checekd, then save to loacal ssttage as [variable]
-//
-// set variable fo rbollian value, have htat value set to local storage.
-
-document.getElementById('messageCheckbox').checked;
-
-var smsChecked = document.getElementById('cboxSms');
-var emailChecked = document.getElementById('cboxEmail');
-
-while (smsChecked == true) {
-  localStorage.setItem('cboxSms', cboxSms);
-// localStorage.setItem('cboxSms', JSON.stringify(cboxSms);
-}
-
-while (emailChecked == true) {
-  localStorage.setItem('cboxEmail', cboxEmail);
-// localStorage.setItem('cboxEmail', JSON.stringify(cboxEmail);
-}
 
 var newUserArray = [];
 var test = document.getElementById('submitButton').addEventListener('click', storedUser);
